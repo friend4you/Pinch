44fb9ab60c0b045ebdcf231431cefda5818aa27e
Vladyslav Arseniuk <vlad.arsenyuk@gmail.com>
2023-06-26 12:27:18 +0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Vladyslav Arseniuk on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
